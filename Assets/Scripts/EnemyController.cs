27a2f91eb45e841105a9071f9d3316524409b3da
YasinZ <yasinzuh@gmail.com>
2017-06-28 12:43:03 -0700

    Added waypoints, spawn controller and enemy controller

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    private int HP = 10;
+
+    private int next = 1;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("MoveToNextWayPoint", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void MoveToNextWayPoint()
+    {
+        if (next == 13)
+            return;
+
+        string point = "point" + next;
+        Debug.Log(point);
+
+        float step = .1f;
+        Vector2 destination = GameObject.Find(point).gameObject.transform.position;
+
+        
+        transform.position = Vector2.MoveTowards(transform.position, destination, step);
+        
+
+        next++;
+        Invoke("MoveToNextWayPoint", 1);
+    }
+
+    private void Wait()
+    {
+        // dead function
+    }
+
+    public int GetHP()
+    {
+        return HP;
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnEnemy", 2, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
+    }
+}
