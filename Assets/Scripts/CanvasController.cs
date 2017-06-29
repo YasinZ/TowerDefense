80b325cd437023cde4eae1728a2d5474840eeeff
YasinZ <yasinzuh@gmail.com>
2017-06-28 18:12:28 -0700

    Added new prefabs and worked on icons

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour {
 
+    public Text tHealth;
+    public Text tMoney;
+    public Text tNext;
+    public Text tTurnCounter;
 
+    private int health = 5;
+    private int money  = 100;
+    private int turnCounter = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+        tHealth.text = "" + health;
+        tNext.text = "Next";
+        tTurnCounter.text = "" + turnCounter;
+        tMoney.text = "" + money;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void TakeDamange()
+    {
+
+    }
 }
