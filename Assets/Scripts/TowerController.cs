e28bb4a30c11be17987abeaaef942e1e82374910
YasinZ <yasinzuh@gmail.com>
2017-06-29 17:56:06 -0700

    Added raycast for bullets + testing scene

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerController : MonoBehaviour {
+    public GameObject bullet;
+
+    private bool canShoot = true;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,30 @@ public class TowerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(canShoot)
+        {
+            Vector2 point = new Vector2(transform.position.x, transform.position.y);
+
+            Collider2D hit = Physics2D.OverlapCircle(point, 1);
+
+            if(hit.CompareTag("Enemy"))
+            {
+                Destroy(hit.gameObject);
+            }
+
+            Cooldown();
+        }
+
 	}
+
+    void Cooldown()
+    {
+        canShoot = false;
+        Invoke("EnableShoot", .5f);
+    }
+
+    void EnableShoot()
+    {
+        canShoot = true;
+    }
 }
